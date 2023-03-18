6a3bc4f75cc9880cfd6a7929845b7403ff1721da
Manohar Kumar Thakur <manoharmkt.182@gmail.com>
2023-03-18 06:23:38 +0530

    design added

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,9 +2,21 @@ import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
 
 const Game = () => {
+  const randomNumbers = Array.from({length: 6}).map(
+    () => 1 + Math.floor(10 * Math.random()),
+  );
+
+  const target = randomNumbers.slice(0, 4).reduce((acc, curr) => acc + curr, 0);
   return (
     <View style={styles.container}>
-      <Text style={styles.target}>42</Text>
+      <Text style={styles.target}>{target} </Text>
+      <View style={styles.randomContainer}>
+        {randomNumbers.map((randomNumber, index) => (
+          <Text key={index} style={styles.random}>
+            {randomNumber}
+          </Text>
+        ))}
+      </View>
     </View>
   );
 };
@@ -13,14 +25,29 @@ export default Game;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#add',
+    backgroundColor: '#ddd',
     flex: 1,
-    paddingTop: 30,
   },
   target: {
-    fontSize: 40,
-    backgroundColor: '#aaa',
-    marginHorizontal: 50,
+    fontSize: 50,
+    margin: 50,
+    backgroundColor: '#bbb',
     textAlign: 'center',
+    borderRadius: 25,
+  },
+  randomContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
+  random: {
+    backgroundColor: '#999',
+    width: 100,
+    marginHorizontal: 15,
+    marginVertical: 25,
+    fontSize: 35,
+    textAlign: 'center',
+    borderRadius: 10,
   },
 });
